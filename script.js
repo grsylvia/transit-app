dd31cad820dc0fee65fa516e047436a779441648
Garrett Sylvia <grsgarrett@gmail.com>
2025-01-07 13:23:15 -0500

    Setting initial longitude and latitude to Boston

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
+const map = L.map('map').setView([42.361, -71.057], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors',
 }).addTo(map);
 
-const marker = L.marker([51.505, -0.09]).addTo(map);
+const marker = L.marker([42.361, -71.057]).addTo(map);
 marker.bindPopup('<b>Hello world!</b><br>I am a popup.').openPopup();
