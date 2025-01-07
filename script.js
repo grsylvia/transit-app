ea6bf7a32a45e31e0bc532f792b748faabc70242
Garrett Sylvia <grsgarrett@gmail.com>
2025-01-07 14:24:55 -0500

    Revert map layer

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const map = L.map('map').setView([42.361, -71.057], 13);
 
-L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}', {
-    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    ext: 'png'
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors',
 }).addTo(map);
 
 const circle = L.circle([42.33129, -71.12641], {
