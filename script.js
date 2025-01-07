0af05e26d9ca49e293e751cb6688e241466432e1
Garrett Sylvia <grsgarrett@gmail.com>
2025-01-07 14:10:38 -0500

    Tweaking opacity and radius of station icons

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 const circle = L.circle([42.33129, -71.12641], {
     color: 'green',
     fillColor: '#00843d',
-    fillOpacity: 0.5,
-    radius: 500
+    fillOpacity: 1.0,
+    radius: 100
 }).addTo(map);
 circle.bindPopup("Brookline Hills").openPopup();
